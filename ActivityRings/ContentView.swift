512e1a1d285f5e9cc0592a136d3b12a59f241919
emt <emir.taletovic@gmail.com>
2020-02-16 16:52:46 -0800

    Initial Commit

---
+++ ActivityRings/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ActivityRings
+//
+//  Created by Emir Taletovic on 2/16/20.
+//  Copyright © 2020 Emir Taletovic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
